6f0186763aa8fcb2ed4bfbe0e464c336bc0f7630
tanish.kumar <tanish.kumar@amantyatech.com>
2024-01-15 21:08:34 +0530

    hs

--- src/LC/January/LC2225.java
+++ src/LC/January/LC2225.java
@@ -16,8 +16,7 @@ public class LC2225 {
             map.put(match[0], map.getOrDefault(match[0],0));
             map.put(match[1], map.getOrDefault(match[1],0)+1);
         }
-
-
+        
         for(int key: map.keySet()){
             if(map.get(key) == 0){
                 winners.add(key);
